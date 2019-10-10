6746894485d2ce58bfd9086d0696c2153746f8ce
creew <123g456H1!>
2019-10-10 17:04:50 +0300

    minor changes

--- fdf/fdf.c
+++ fdf/fdf.c
@@ -1,8 +1,17 @@
 
 #include "fdf.h"
+#include "mlx.h"
 
-int main(int ac, char *av[])
+int		main(int ac, char *av[])
 {
+	void *mlx_data;
+	void *mlx_window;
 
-    return (0);
+	mlx_data = mlx_init();
+	if (mlx_data)
+	{
+		mlx_window = mlx_new_window(mlx_data, 1000,1000, "Превед!");
+		mlx_loop(mlx_data);
+	}
+	return (0);
 }
