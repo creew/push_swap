3f18dc4449991544cb0816528c9ad39cab1c6fcf
creew <eklompus@student.21-school.ru>
2019-10-31 19:38:47 +0300

    new push_swap new algo

--- push_swap2/push_swap/push_swap.c
+++ push_swap2/push_swap/push_swap.c
@@ -19,6 +19,65 @@ int		find_optimal(t_stack *st1, t_stack *st2, int *count)
 
 }
 
+int		is_cycle_sorted(t_stack *st, int start)
+{
+	int	end;
+	int prev_val;
+
+	prev_val = st->stack[start];
+	end = start == 0 ? 0 : start;
+	start--;
+	while (start != end)
+	{
+		if (prev_val > st->stack[start])
+			return (0);
+		prev_val = st->stack[start];
+
+		start = start == 0 ? (int)st->pos - 1 : start - 1;
+	}
+	return (1);
+}
+
+int 	find_min_index(t_stack *st)
+{
+	size_t	count;
+	int		min;
+	int		blocked;
+
+	blocked = 0;
+	min = st->pos ? 0 : -1;
+	count = 0;
+	while (++count < st->pos)
+	{
+		if (st->stack[count] < st->stack[min])
+			min = (int)count;
+		else if (st->stack[count] == st->stack[min])
+		{
+
+		}
+	}
+	return (min);
+}
+
+void	sort3items(t_stack *st, int *count)
+{
+	int	min_index;
+
+	if (st->pos == 2)
+	{
+		if (!is_stack_sorted(st, 2, 1))
+			run_commands(st, NULL, S_SA, count);
+	}
+	if (st->pos == 3)
+	{
+		if ((min_index = find_min_index(st)) != -1)
+		{
+			if (!is_cycle_sorted(st, min_index))
+				run_commands(st, NULL, S_SA, count);
+		}
+	}
+}
+
 int		calc_optimal(t_stack *st1, t_stack *st2)
 {
 	size_t	size;
@@ -28,8 +87,8 @@ int		calc_optimal(t_stack *st1, t_stack *st2)
 	size = st1->pos;
 	while (size-- > 3)
 		run_commands(st1, st2, S_PB, &count);
-
-
+	sort3items(st1, &count);
+	return (count);
 }
 
 int		main(int ac, char *av[])
