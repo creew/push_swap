25199409ace386c6c2d0ef1544137f1558d723d8
creew <eklompus@student.21-school.ru>
2019-11-01 19:53:42 +0300

    push_swap fixed

--- push_swap2/push_swap/push_swap.c
+++ push_swap2/push_swap/push_swap.c
@@ -12,11 +12,51 @@
 
 #include "push_swap.h"
 
-int		find_optimal(t_stack *st1, t_stack *st2, int *count)
+
+int		paste_optimal(t_stack *st1, t_stack *st2)
 {
+	(void)st1;
+	(void)st2;
+
+
+	return (0);
+}
+
+int		find_place_to_paste(t_stack *st1, int val)
+{
+	size_t	size;
+	size_t	pos;
+
+	pos = st1->pos - 1;
+	size = st1->pos;
+	while (size--)
+	{
+
+
+	}
 
 
 
+}
+
+int		find_optimal(t_stack *st1, t_stack *st2)
+{
+	(void)st1;
+	int		size;
+	int		val;
+	int		s2;
+	int		s1;
+
+	size = (int)st2->pos;
+	while (size--)
+	{
+		val = st2->stack[size];
+		s2 = ft_min((int)st2->pos - 1 - size, size + 1);
+		s1 = find_place_to_paste(st1, val);
+
+
+	}
+	return (0);
 }
 
 int		is_cycle_sorted(t_stack *st, int start)
@@ -26,13 +66,12 @@ int		is_cycle_sorted(t_stack *st, int start)
 
 	prev_val = st->stack[start];
 	end = start == 0 ? 0 : start;
-	start--;
+	start = start == 0 ? (int)st->pos - 1 : start - 1;
 	while (start != end)
 	{
 		if (prev_val > st->stack[start])
 			return (0);
 		prev_val = st->stack[start];
-
 		start = start == 0 ? (int)st->pos - 1 : start - 1;
 	}
 	return (1);
@@ -45,16 +84,25 @@ int 	find_min_index(t_stack *st)
 	int		blocked;
 
 	blocked = 0;
-	min = st->pos ? 0 : -1;
-	count = 0;
-	while (++count < st->pos)
+	min = st->pos ? (int)st->pos - 1 : -1;
+	count = st->pos;
+	while (count--)
 	{
 		if (st->stack[count] < st->stack[min])
+		{
 			min = (int)count;
+			blocked = 1;
+		}
 		else if (st->stack[count] == st->stack[min])
 		{
-
+			if (!blocked)
+			{
+				min = (int)count;
+				blocked = 1;
+			}
 		}
+		else
+			blocked = 0;
 	}
 	return (min);
 }
@@ -78,6 +126,30 @@ void	sort3items(t_stack *st, int *count)
 	}
 }
 
+int 	normalize_stack(t_stack *st, int *count)
+{
+	int	min;
+	int	n;
+
+	min = find_min_index(st);
+	if (min != (int)st->pos - 1)
+	{
+		if ((int)st->pos - 1 - min < min - 0 + 1)
+		{
+			n = (int)st->pos - 1 - min;
+			while (n--)
+				run_commands(st, NULL, S_RA, count);
+		}
+		else
+		{
+			n = min - 0 + 1;
+			while (n--)
+				run_commands(st, NULL, S_RRA, count);
+		}
+	}
+	return (0);
+}
+
 int		calc_optimal(t_stack *st1, t_stack *st2)
 {
 	size_t	size;
@@ -88,6 +160,12 @@ int		calc_optimal(t_stack *st1, t_stack *st2)
 	while (size-- > 3)
 		run_commands(st1, st2, S_PB, &count);
 	sort3items(st1, &count);
+	while (st2->pos)
+	{
+		find_optimal(st1, st2);
+		paste_optimal(st1, st2);
+	}
+	normalize_stack(st1, &count);
 	return (count);
 }
 
